ecef9d2a246ff0f74d95aa57c050822c98cc75fd
kznr <who.is.kazunari@gmail.com>
2019-12-02 22:54:54 +0900

    Resolve issue #13

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setuptools.setup(
     download_url='https://pypi.org/project/harvesters_gui/',
     # A list of required Python modules:
     install_requires=[
-        'PyQt5',
+        'PyQt5<=5.13',
         'vispy<=0.6',
         'harvesters>=0.2.4,<=0.2.12',
     ],
