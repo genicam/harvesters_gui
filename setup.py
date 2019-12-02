543c1a1f07fd0bc8285b3fa137ef6dd7a00724a8
kznr <who.is.kazunari@gmail.com>
2019-12-02 22:34:17 +0900

    Resolve issue #13

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setuptools.setup(
     # A list of required Python modules:
     install_requires=[
         'PyQt5<=5.13',
-        'vispy<=0.6'
+        'vispy<=0.6',
         'harvesters>=0.2.4,<=0.2.12',
     ],
 
