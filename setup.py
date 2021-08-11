98883ded4e6db35c8b646d4314b5c4c191f71cb2
kznr <who.is.kazunari@gmail.com>
2021-08-12 02:37:03 +0900

    Update the supported Python versions

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ log.info('$PATH=%s' % os.environ['PATH'])
 
 
 # Check the Python version:
-supported_versions = [(3, 4), (3, 5), (3, 6)]
+supported_versions = [(3, 5), (3, 6), (3, 7), (3, 8)]
 if sys.version_info in supported_versions:
     raise RuntimeError(
         'See https://github.com/genicam/harvesters_gui#requirements'
@@ -70,10 +70,10 @@ setuptools.setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ),
     # A short, on-sentence summary of the package:
     description=description,
